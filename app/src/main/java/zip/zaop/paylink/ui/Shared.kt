bd95a2d2a3fc191e3af57d068692c4a0d61c5251
zaop <11255568+enteryournamehere@users.noreply.github.com>
2023-11-05 17:17:16 +0100

    Copyable alerts

--- app/src/main/java/zip/zaop/paylink/ui/Shared.kt
+++ app/src/main/java/zip/zaop/paylink/ui/Shared.kt
@@ -1,6 +1,7 @@
 package zip.zaop.paylink.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,6 +15,8 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,6 +25,8 @@ import zip.zaop.paylink.ui.theme.PaylinkTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Alert(title: String, content: String, onDismiss: () -> Unit) {
+    val clipboardManager = LocalClipboardManager.current
+    var copyButtonText = "copy message"
     AlertDialog(
         onDismissRequest = { onDismiss() },
     ) {
@@ -37,13 +42,24 @@ fun Alert(title: String, content: String, onDismiss: () -> Unit) {
                 Text(title, fontSize = 20.sp)
                 Spacer(Modifier.height(10.dp))
                 Text(content)
-                TextButton(
-                    onClick = {
-                        onDismiss()
-                    },
-                    modifier = Modifier.align(Alignment.End),
+                Row(
+                    modifier = Modifier.align(Alignment.End)
                 ) {
-                    Text("OK")
+                    TextButton(
+                        onClick = {
+                            clipboardManager.setText(AnnotatedString(content))
+                            copyButtonText = "copied"
+                        },
+                    ) {
+                        Text(copyButtonText)
+                    }
+                    TextButton(
+                        onClick = {
+                            onDismiss()
+                        },
+                    ) {
+                        Text("ok")
+                    }
                 }
             }
         }
